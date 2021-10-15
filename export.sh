86d80c9f81d17844bf7c450cd50d617e55b520c4
Jonathan de Montalembert <jonathan.demontalembert2@auspost.com.au>
2021-10-15 20:09:25 +1100

    export dotfile

--- export.sh
+++ export.sh
@@ -5,6 +5,14 @@ export DOTFILES_PATH=${CURRENT_FILE_PATH%"/."}
 
 echo "👉 dotfiles path: '$DOTFILES_PATH'"
 echo ""
+echo "👇 Export dotfiles 👇"
+echo ""
+cp ~/.zshrc $DOTFILES_PATH/zsh/.zshrc
+cp ~/.p10k $DOTFILES_PATH/zsh/.p10k
+cp ~/.oh-my-zsh/custom/aliases.zsh $DOTFILES_PATH/zsh/aliases.zsh
+cp ~/.gitconfig $DOTFILES_PATH/git/.gitconfig
+cp ~/.gitignore_global $DOTFILES_PATH/git/.gitignore_global
+cp ~/.vimrc $DOTFILES_PATH/.vimrc
 echo "👇 Export Brewfile 👇"
 echo ""
 brew bundle dump --file=./mac/brew/Brewfile --force
